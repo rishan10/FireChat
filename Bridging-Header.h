170438b2c28ad91f05a967d57b2d8b035fa45e43
Rishan Girish <rishangirish@MacBook-Pro.local>
2017-09-08 01:17:47 -0700

    added header, first steps integrating Firebase

---
+++ Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  FireChat
+//
+//  Created by Rishan Girish on 9/8/17.
+//  Copyright © 2017 Rishan Girish. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
+#import <JSQMessagesViewController/JSQMessages.h>
